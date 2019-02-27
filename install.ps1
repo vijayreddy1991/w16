3ddeea6fb4792cec4c2ace524cd5a11f2b9fdd29
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2019-02-27 15:33:39 +0530

    update the path for git

--- install.ps1
+++ install.ps1
@@ -29,12 +29,8 @@ Install-ChocoPackage nodejs-lts
 Install-ChocoPackage git
 Update-SessionEnvironment
  
-# Git and friends
-# git-lfs includes git as a dependency so there is no need to install it explicitly
-Install-ChocoPackage git-lfs
-
 $ENV:PATH = 'C:\Program Files\Git\bin;' + $env:PATH
-$ENV:PATH = 'C:\Program Files\Git LFS;' + $env:PATH
+
 
 # Java 11
 Install-ChocoPackage jdk11
